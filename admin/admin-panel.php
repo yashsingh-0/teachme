380e47838a97cb82926ff9d97d3f7a1fc0cc7fcc
Yash Singh <yashkumar2566@gmail.com>
2021-11-11 18:28:57 +0530

    Update admin-panel.php

--- admin/admin-panel.php
+++ admin/admin-panel.php
@@ -381,22 +381,7 @@
                     <!--/table Card-->
                 </div>
 
-                <div class="w-full md:w-1/2 xl:w-1/3 p-6">
-                    <!--Advert Card-->
-                    <div class="bg-white border-transparent rounded-lg shadow-xl">
-                        <div class="bg-gradient-to-b from-gray-300 to-gray-100 uppercase text-gray-800 border-b-2 border-gray-300 rounded-tl-lg rounded-tr-lg p-2">
-                            <h5 class="font-bold uppercase text-gray-600">Advert</h5>
-                        </div>
-                        <div class="p-5 text-center">
-
-
-                            <script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?serve=CK7D52JJ&placement=wwwtailwindtoolboxcom" id="_carbonads_js"></script>
-
-
-                        </div>
-                    </div>
-                    <!--/Advert Card-->
-                </div>
+                
 
 
             </div>
